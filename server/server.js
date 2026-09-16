af5ecbca95b724a98fe939e9c21a98829e510355
zobiammasood <zobiamasood105@gmail.com>
2026-09-16 12:58:20 +0500

    Fix CORS for deployed frontend

--- server/server.js
+++ server/server.js
@@ -10,8 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/StudyVault';
-
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({
+  origin: 'https://study-vault-delta.vercel.app'
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
